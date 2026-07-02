63dd4bc673c3439d21957766c9c04e288593b955
Santiago072 <santiagolizcanosuarez@gmail.com>
2026-07-02 15:18:59 -0500

    fix: remover referencia a syncRoutes inexistente que crasheaba el servidor

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const syncRoutes = require('./routes/syncRoutes');
 const authRoutes = require('./routes/authRoutes');
 const encuestadoresRoutes = require('./routes/encuestadoresRoutes');
 const apiRoutes = require('./routes/api');
@@ -14,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/sync', syncRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/encuestadores', encuestadoresRoutes);
 app.use('/api', apiRoutes);
